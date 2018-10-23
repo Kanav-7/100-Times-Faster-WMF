89dd828c32d590fd50170872f37af277b8cc267a
garunaX <garuna.athem@gmail.com>
2018-10-23 12:46:59 +0530

    missed global

---
+++ global.hpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <string>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <time.h>
+
+using namespace std;
+using namespace cv;
