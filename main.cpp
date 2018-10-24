052cce756f8dc5e43fae1653fe3b1d927c9b2267
kanav-7 <kanavgupta0711@gmail.com>
2018-10-24 09:13:13 +0530

    let the code begin

--- main.cpp
+++ main.cpp
@@ -8,12 +8,18 @@ int** int2D(int dim1, int dim2){
 	return ret;
 }
 
+// Mat req_filter(Mat &I, Mat &feature, int r)
+// {
+
+
+// }
+
 int main( int argc, char** argv ) {
 
 	cv::Mat image,imagegray;
 	image = imread("xy.png" , CV_LOAD_IMAGE_COLOR);
 	cvtColor(image,imagegray, CV_BGR2GRAY);
-	imshow("Display window", MediamBlur(imagegray,imagegray,1));
+	imshow("Display window",req_filter(imagegray,imagegray,1));
 	waitKey(0);
 	return 0;
 }
