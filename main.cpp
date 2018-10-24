230ea0f4766e5680f579ba560d55e980da7d77d2
garuna-m6 <anu01.m@gmail.com>
2018-10-25 05:28:36 +0530

    out of brains

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ int** int2D(int dim1, int dim2){
 	return ret;
 }
 
-Mat req_filter(Mat &I, Mat &feature, int r)
+Mat req_filter(Mat &I, int r)
 {
+	Mat feature = I.clone();
 	Mat F = feature.clone();
 	int nI = 256,nF = 256, weightType = 1;
 	I.convertTo(I,CV_32S);
@@ -22,12 +23,20 @@ Mat req_filter(Mat &I, Mat &feature, int r)
 	nF = 256;
 	F.convertTo(FNew, CV_32S);
 
-		float **ret = new float*[nF];
+	float **ret = new float*[nF];
 	ret[0] = new float[nF*nF];
 	for(int i=1;i<nF;i++)
 		ret[i] = ret[i-1]+nF;
 
 	wMap =  ret;
+	for(int i=0;i<nF;i++){
+		for(int j=i;j<nF;j++){
+			float diff = fabs((float)(i-j));
+			if(op==1)wMap[i][j] = wMap[j][i] = exp(-(diff*diff)*denom);
+			else wMap[i][j] = wMap[j][i] = 1.0f;
+		}
+	}
+	F = FNew;
 
 
 	return F;
@@ -39,7 +48,7 @@ int main( int argc, char** argv ) {
 	cv::Mat image,imagegray;
 	image = imread("xy.png" , CV_LOAD_IMAGE_COLOR);
 	cvtColor(image,imagegray, CV_BGR2GRAY);
-	imshow("Display window",req_filter(imagegray,imagegray,1));
+	imshow("Display window",req_filter(imagegray,1));
 	waitKey(0);
 	return 0;
 }
