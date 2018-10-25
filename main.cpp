dc8931aa464175d4c013e8efe86bc5c6501e416a
garuna-m6 <anu01.m@gmail.com>
2018-10-25 22:57:49 +0530

    created the histogram, maybe

--- main.cpp
+++ main.cpp
@@ -21,6 +21,57 @@ Mat filterCore(Mat &I, Mat &F, float **wMap,int r)
 		mask = Mat(I.size(),CV_8U);
 		mask = Scalar(1);
 	}
+		int **H = int2D(nI,nF);
+	int *BCB = new int[nF];
+
+	int **Hf = int2D(nI,nF), **Hb = int2D(nI,nF);
+	int *BCBf = new int[nF],*BCBb = new int[nF];
+
+	for(int x=0;x<cols;x++){
+		memset(BCB, 0, sizeof(int)*nF);
+		memset(H[0], 0, sizeof(int)*nF*nI);
+		for(int i=0;i<nI;i++)
+			Hf[i][0]=Hb[i][0]=0;
+		BCBf[0]=BCBb[0]=0;
+
+		int medianVal = -1;
+		int downX = max(0,x-r);
+		int upX = min(cols-1,x+r);
+
+		// Initialize joint-histogram
+		int upY = min(rows-1,r);
+		for(int i=0;i<=upY;i++){
+
+			int *IPtr = I.ptr<int>(i);
+			int *FPtr = F.ptr<int>(i);
+			uchar *maskPtr = mask.ptr<uchar>(i);
+
+			for(int j=downX;j<=upX;j++){
+
+				if(!maskPtr[j])continue;
+
+				int fval = IPtr[j];
+				int *curHist = H[fval];
+				int gval = FPtr[j];
+
+				// necklace table
+				if(!curHist[gval] && gval){
+					int *curHf = Hf[fval];
+					int *curHb = Hb[fval];
+
+					int p1=0,p2=curHf[0];
+					curHf[p1]=gval;
+					curHf[gval]=p2;
+					curHb[p2]=gval;
+					curHb[gval]=p1;
+				}
+
+				curHist[gval]++;
+				updateBCB(BCB[gval],BCBf,BCBb,gval,-1);
+			}
+		}
+
+	return curHist;
 }
 
 Mat req_filter(Mat &I, int r)
