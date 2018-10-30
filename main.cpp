d7a6a596b6e3a694b64c0d3e2750d11fff7a73bd
garuna-m6 <anu01.m@gmail.com>
2018-10-30 23:47:16 +0530

    atleast working and refactored a lot

--- main.cpp
+++ main.cpp
@@ -1,162 +1,7 @@
 #include "global.hpp"
+#include "core.hpp"
 #include "allocint.hpp"
-
-
-Mat filterCore(Mat &I, Mat &F, float **wMap,int r)
-{
-	int nF=256,nI=256;
-	Mat mask = Mat();
-	int rows = I.rows, cols = I.cols;
-	int alls = rows * cols;
-	int winSize = (2*r+1)*(2*r+1);
-	Mat outImg = I.clone();
-
-	if(mask.empty()){
-		mask = Mat(I.size(),CV_8U);
-		mask = Scalar(1);
-	}
-		int **H = int2D(nI,nF);
-	int *BCB = new int[nF];
-
-	int **Hf = int2D(nI,nF), **Hb = int2D(nI,nF);
-	int *BCBf = new int[nF],*BCBb = new int[nF];
-
-	for(int x=0;x<cols;x++){
-		memset(BCB, 0, sizeof(int)*nF);
-		memset(H[0], 0, sizeof(int)*nF*nI);
-
-		BCBf[0]=BCBb[0]=0;
-
-		int medianVal = -1;
-		int downX = max(0,x-r);
-		int upX = min(cols-1,x+r);
-
-		// Initialize joint-histogram
-		int upY = min(rows-1,r);
-		for(int i=0;i<=upY;i++){
-
-			int *IPtr = I.ptr<int>(i);
-			int *FPtr = F.ptr<int>(i);
-			uchar *maskPtr = mask.ptr<uchar>(i);
-
-			for(int j=downX;j<=upX;j++){
-
-				if(!maskPtr[j])continue;
-
-				int fval = IPtr[j];
-				int *curHist = H[fval];
-				int gval = FPtr[j];
-
-				// necklace table
-				if(!curHist[gval] && gval){
-					int *curHf = Hf[fval];
-					int *curHb = Hb[fval];
-
-					int p1=0,p2=curHf[0];
-					curHf[p1]=gval;
-					curHf[gval]=p2;
-					curHb[p2]=gval;
-					curHb[gval]=p1;
-				}
-
-				curHist[gval]++;
-				updateBCB(BCB[gval],BCBf,BCBb,gval,-1);
-			}
-		}
-		for(int y=0;y<rows;y++)
-		{
-			{
-
-				float balw = 0;
-				int curIndex = F.ptr<int>(y,x)[0];
-				float *fPtr = wMap[curIndex];
-				int &cmedval = medianVal;
-				int i=0;
-				do{
-					balw += BCB[i]*fPtr[i];
-					i=BCBf[i];
-				}while(i);
-
-				if(balw >= 0){
-					for(balw;balw >= 0 && cmedval;cmedval--){
-						float curWeight = 0;
-						int *nextHist = H[cmedval];
-						int *nextHf = Hf[cmedval];
-
-						int i=0;
-						do{
-							curWeight += (nextHist[i]<<1)*fPtr[i];
-							
-							updateBCB(BCB[i],BCBf,BCBb,i,-(nextHist[i]<<1));
-							
-							i=nextHf[i];
-						}while(i);
-
-						balw -= curWeight;
-					}
-				}
-				else if(balw < 0){
-					for(balw;balw < 0 && cmedval != nI-1; cmedval++){
-						float curWeight = 0;
-						int *nextHist = H[cmedval+1];
-						int *nextHf = Hf[cmedval+1];
-
-						int i=0;
-						do{
-							curWeight += (nextHist[i]<<1)*fPtr[i];
-
-							updateBCB(BCB[i],BCBf,BCBb,i,nextHist[i]<<1);
-							
-							i=nextHf[i];
-						}while(i);
-						balw += curWeight;
-					}
-				}
-
-				if(balw<0)outImg.ptr<int>(y,x)[0] = cmedval+1;
-				else outImg.ptr<int>(y,x)[0] = cmedval;
-			}
-
-			rownum = y - r;
-			if(rownum >= 0){
-
-				int *inpptr = I.ptr<int>(rownum);
-				int *gdptr = F.ptr<int>(rownum);
-				uchar *maskPtr = mask.ptr<uchar>(rownum);
-
-				for(int j=downX;j<=upX;j++){
-
-					if(!maskPtr[j])continue;
-
-					fval = inpptr[j];
-					curHist = H[fval];
-					gval = gdptr[j];
-
-					curHist[gval]--;
-					if(!curHist[gval] && gval){
-						int *curHf = Hf[fval];
-						int *curHb = Hb[fval];
-
-						int p1=curHb[gval],p2=curHf[gval];
-						curHf[p1]=p2;
-						curHb[p2]=p1;
-					}
-					updateBCB(BCB[gval],BCBf,BCBb,gval,-((fval <= medianVal)<<1)+1);
-				}
-			}
-		}
-
-	}
-
-	return outImg;
-}
-int** int2D(int dim1, int dim2){
-	int **ret = new int*[dim1];
-	ret[0] = new int[dim1*dim2];
-	for(int i=1;i<dim1;i++)
-		ret[i] = ret[i-1]+dim2;
-	return ret;
-}
+#include "bcb.hpp"
 
 Mat req_filter(Mat &I, int r)
 {
@@ -167,7 +12,6 @@ Mat req_filter(Mat &I, int r)
 
 	float **wMap;
 	Mat FNew;
-
 	int alls = F.cols * F.rows;
 	int op;
 	nF = 256;
@@ -179,6 +23,9 @@ Mat req_filter(Mat &I, int r)
 		ret[i] = ret[i-1]+nF;
 
 	wMap =  ret;
+
+	float denom = (1.0f/(2*26*26));
+
 	for(int i=0;i<nF;i++){
 		for(int j=i;j<nF;j++){
 			float diff = fabs((float)(i-j));
@@ -188,10 +35,10 @@ Mat req_filter(Mat &I, int r)
 	}
 	F = FNew;
 
+	//Filtering - Joint-Histogram Framework
 	I = filterCore(I, F, wMap, r);
 	I.convertTo(I,CV_8U);
-	return F;
-
+	return I;
 }
 
 int main( int argc, char** argv ) {
@@ -199,7 +46,7 @@ int main( int argc, char** argv ) {
 	cv::Mat image,imagegray;
 	image = imread("xy.png" , CV_LOAD_IMAGE_COLOR);
 	cvtColor(image,imagegray, CV_BGR2GRAY);
-	imshow("Display window",req_filter(imagegray,1));
+	imshow("Display window", req_filter(imagegray,1));
 	waitKey(0);
 	return 0;
 }
